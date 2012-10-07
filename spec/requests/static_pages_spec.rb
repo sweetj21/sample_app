6152da517ecff0ccc532958408bfd1e76140b8cb
Eric <eric.millington@gmail.com>
2012-10-07 16:44:12 -0400

    Contact and About pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,8 +6,47 @@ describe "Home page" do
 it "should have the content 'Sample App'" do
 
 visit '/static_pages/home'
-page.should have_content('Sample App')
+page.should have_selector('h1', :text => 'Sample App')
+end
 
+it "should have the right title" do
+	visit '/static_pages/home'
+page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+end
+end
+end
+
+describe "StaticPages" do
+
+describe "Help page" do
+it "should have the content 'Help'" do
+
+visit '/static_pages/help'
+page.should have_selector('h1', :text => 'Help')
+
+end
+it "should have the right title" do
+	visit '/static_pages/help'
+page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+end
+end
+end
+
+
+
+
+describe "StaticPages" do
+
+describe "Contact page" do
+it "should have the content 'Contact'" do
+
+visit '/static_pages/contact'
+page.should have_selector('h1', :text => 'Contact')
+
+end
+it "should have the right title" do
+	visit '/static_pages/contact'
+page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
 end
 end
 end
