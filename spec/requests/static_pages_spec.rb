4e7321624d220af4b0d9a02efb1313f19d8c2b51
Eric <eric.millington@gmail.com>
2012-10-07 16:47:19 -0400

    fixed rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,7 +33,21 @@ end
 end
 
 
+describe "StaticPages" do
 
+describe "About page" do
+it "should have the content 'About'" do
+
+visit '/static_pages/about'
+page.should have_selector('h1', :text => 'About Us')
+
+end
+it "should have the right title" do
+	visit '/static_pages/about'
+page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+end
+end
+end
 
 describe "StaticPages" do
 
